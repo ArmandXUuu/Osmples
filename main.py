2381dc165409103f599b78e979444b018e45f4cd
Armand XU <ziyi_xu@icloud.com>
2021-11-11 11:01:24 +0100

    a little reformat

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
 #!/usr/bin/python3
+
+import os
+
+
+# Global variables
+terminate_programe = False
+
+
 def show_welcoming_message():
     print("Bonjour ô maître Rémi ! Comment puis-je aider votre Sainteté ?")
 
+
 def show_options():
-    print("""->1<- Créer un vote.\n->2<- Enregistrer un électeur.\n->3<- Enregistrer un vote.\n->4<- Vérifier un vote.\n->5<- Procéder au dépouillement.\n->6<- Quitter le programme.""")
+    print(
+        "->1<- Créer un vote.\n->2<- Enregistrer un électeur.\n->3<- Enregistrer un vote.\n->4<- Vérifier un vote.\n->5<- Procéder au dépouillement.\n->6<- Quitter le programme.")
+
 
 def start_vote_creation():
-    print("Création du vote ...")
+    print_information("Création du vote ...")
+
 
 def start_elector_creation():
-    print("Création d'un électeur...")
+    print_information("Création d'un électeur...")
+
 
 def start_vote_selection():
-    print("Sélection d'un vote...")
+    print_information("Sélection d'un vote...")
+
 
 def start_vote_verification():
-    print("Vérification du vote...")
+    print_information("Vérification du vote...")
+
 
 def start_vote_counting():
-    print("Dépouillement du vote...")
+    print_information("Dépouillement du vote...")
+
+
+def terminate_program():
+    global terminate_programe
+    terminate_programe = True
+    print("\033[1;5;31m\tAu revoir !\033[0m")
+
+
+def print_information(message: str):
+    print("\033[32m%s\033[0m" % message)
+
+
+def execute_option(index: str):
+    options = {
+        "1": start_vote_creation,
+        "2": start_elector_creation,
+        "3": start_vote_selection,
+        "4": start_vote_verification,
+        "5": start_vote_counting,
+        "6": terminate_program
+    }
+
+    method = options.get(index)
+    if method:
+        method()
+    else:
+        print("\033[1;5;31m\n\tEntrée invalide ;)\n\tOn recommence !\n\n\033[0m")  # https://www.cnblogs.com/wj-1314/p/7449812.html
+        os.system("sleep 2 && clear")
+
 
 def main():
     show_welcoming_message()
-    has_been_terminated = False
-    while not has_been_terminated:
+    while not terminate_programe:
         show_options()
-        chosen_option = str(input())
-        if "1" == chosen_option:
-            start_vote_creation()
-        elif "2" == chosen_option:
-            start_elector_creation()
-        elif "3" == chosen_option:
-            start_vote_selection()
-        elif "4" == chosen_option:
-            start_vote_verification()
-        elif "5" == chosen_option:
-            start_vote_counting()
-        elif "6" == chosen_option:
-            has_been_terminated = True
+        chosen_option = input("Faites votre choix : ")
+        execute_option(chosen_option)
 
-main()
+
+if __name__ == "__main__":
+    main()
