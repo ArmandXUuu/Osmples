eb4dcc146dcce64e31cc2424aa107feae2341f12
Armand XU <ziyi_xu@icloud.com>
2021-11-18 15:52:49 +0100

    init

--- classes/RegistrationServer.py
+++ classes/RegistrationServer.py
@@ -4,10 +4,10 @@
 from typing import List
 from classes.Server import Server
 from classes.AdministrationServer import AdministrationServer as A
-import pbkdf2 as PUB
 from utils.log_util import logger
 from random import shuffle
 from cryptoUtils.hashage import generate_uuid
+from Crypto.Protocol.KDF import PBKDF2
 
 class RegistrationServer(Server):
 
@@ -15,14 +15,15 @@ class RegistrationServer(Server):
         print("initiate a Serveur d'enregistrement")
 
     def generate_credentials(self, uuid_list: list):
-        vote_codes = {}
+        vote_codes = []
 
         for uuid in uuid_list:
-            f = open("/data/credentials/" + uuid, "w")
-            f.write(self.generate_secret_id(uuid))
-            vote_codes[uuid] = self.__generate_vote_code(uuid)
+            c_n = self.generate_secret_id(uuid)
+            f = open("./data/credentials/" + uuid, "w")
+            f.write(c_n)
+            vote_codes.append(self.__generate_vote_code(c_n, uuid))
             f.close()
-        return vote_codes
+        return self.__shuffle_list(vote_codes)
 
     def generate_secret_id(self, uuid: str) -> str:
         c_n = generate_uuid(uuid)
@@ -30,10 +31,11 @@ class RegistrationServer(Server):
         return c_n
 
     @staticmethod
-    def __generate_vote_code(uuid: str) -> str:  # Pub() function
+    def __generate_vote_code(c_n: str, uuid: str) -> bytes:  # Pub() function
         print("Code de vote pour c_n is generated")
-        logger.debug("TODO generate Pub(c_n) for {}".format(uuid))
-        return "TODO {} + stuff = Pub(c_n)".format(uuid)
+       # logger.debug("TODO generate Pub(c_n) for {}".format(uuid))
+        res = PBKDF2(c_n, bytes(uuid, "utf-8"))
+        return PBKDF2(c_n, bytes(uuid, "utf-8"))
 
     def __shuffle_list(self, list_in: [str]) -> [str]:
         list_out = list_in
--- classes/classUnitTest.py
+++ classes/classUnitTest.py
@@ -13,15 +13,21 @@ class MyTestCase(unittest.TestCase):
         e = E()
         s = S()
 
-        user1 = User("Ziyi", "XU", "ziyi@abc.abc", UserTypes.Voter)
-        user2 = User("ThangLong", "C", "abc@abc.abc", UserTypes.Voter)
+        user1 = User("Ziyi", "XU", "ziyi@avbc.abc", UserTypes.Voter)
+        user2 = User("ThangLdong", "C", "abc@abc.abc", UserTypes.Voter)
+        user3 = User("ThanagLong", "C", "abc@avbc.abc", UserTypes.Voter)
+        user4 = User("Than5gLdong", "C", "abc@abc.abc", UserTypes.Voter)
+        user5 = User("ThanzgLong", "C", "abc@abc.abc", UserTypes.Voter)
 
         a.add_user(user1)
         a.add_user(user2)
+        a.add_user(user3)
+        a.add_user(user4)
+        a.add_user(user5)
         vote_test = V(["Macron", "Obama", "XI Jinping"], "2021-11-11 00:00:00", "2022-01-01 12:59:59", 1)
         print(vote_test)
 
-        e.generate_credentials(a.get_uuids())
+        print(e.generate_credentials(a.get_uuids()))
 
         e.set_certificate("TODO, certificate to generate while init, TODO to rename to a identifiable string")
 
